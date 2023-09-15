0b7cc7c3fecdd4ac8edca4febbc884169dec8801
camili7 <109960784+camili7@users.noreply.github.com>
2023-09-15 11:46:08 -0300

    Add files via upload

---
+++ header.cs
@@ -0,0 +1,23 @@
+.cabeçalho__menu-hamburguer {
+
+    width: 24px;
+    height: 24px;
+    background-image: url("../img/menu.svg");
+    background-repeat: no-repeat;
+    background-position: center;
+    display: inline-block;
+}
+.cabeçalho {
+
+    background-color: var(--branco);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.container {
+    display: flex;
+    align-items: center;
+}
+.container__imagem {
+    padding: 1em;
+}
